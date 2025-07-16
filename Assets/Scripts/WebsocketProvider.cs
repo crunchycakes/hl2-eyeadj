edaaa5d807d25323b79a6f3a53822012a294b9d9
crunchycakes <85715474+crunchycakes@users.noreply.github.com>
2025-07-16 04:00:32 -0600

    all needed axes

--- Assets/Scripts/WebsocketProvider.cs
+++ Assets/Scripts/WebsocketProvider.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using WebSocketSharp;
 using Newtonsoft.Json.Linq;
+using System;
 
 public class WebsocketProvider : MonoBehaviour
 {
 
     private WebSocket ws;
 
-    public float sliderValue;
+    public float sliderValuey;
+    public float sliderValuex;
+    public float sliderValued;
+    public float sliderValuea;
+    public GameObject pv;
 
     void Start()
     {
@@ -32,8 +37,40 @@ public class WebsocketProvider : MonoBehaviour
             JObject msg = JObject.Parse(e.Data);
             if ((string)msg["type"] == "slider")
             {
-                sliderValue = (float)msg["value"];
-                Debug.Log("slider value recieved: " + sliderValue);
+                string sliderId = (string)msg["id"];
+                float val = (float)msg["value"];
+
+                if (sliderId == "sliderx")
+                {
+                    sliderValuex = val;
+                    Debug.Log("slider x updated: " + sliderValuex);
+                }
+                else if (sliderId == "slidery")
+                {
+                    sliderValuey = val;
+                    Debug.Log("slider y updated: " + sliderValuey);
+                }
+                else if (sliderId == "sliderd")
+                {
+                    sliderValued = val;
+                    Debug.Log("slider d updated: " + sliderValued);
+                }
+                else if (sliderId == "slidera")
+                {
+                    sliderValuea = val;
+                    Debug.Log("slider a updated: " + sliderValuea);
+                }
+
+                // GET RID OF THESE MAGIC NUMS!!!
+                //try
+                //{
+                //    pv.transform.localPosition = new Vector3((sliderValuex - 50) * 0.0025f, (sliderValuey - 50) * 0.0005f, 0.15f);
+                //}
+                //catch (Exception ere)
+                //{
+                //    Debug.LogError(ere.Message);
+                //}
+                //Debug.Log("Transform success!?");
             }
         };
 
@@ -41,6 +78,9 @@ public class WebsocketProvider : MonoBehaviour
         ws.OnError += (sender, e) =>
         {
             Debug.LogError("!!!!! websocket error: " + e.Message);
+            Debug.Log(sliderValuex);
+            Debug.Log(sliderValuey);
+            Debug.Log(sliderValued);
         };
 
         // Called when closed
@@ -52,14 +92,18 @@ public class WebsocketProvider : MonoBehaviour
         ws.ConnectAsync();  // Non-blocking connect
     }
 
-    //void Update()
-    //{
-    //    // Optional: Ping server every 30s to keep alive (Heroku idle timeout)
-    //    if (ws != null && ws.IsAlive)
-    //    {
-    //        // Keep-alive logic if needed
-    //    }
-    //}
+    void Update()
+    {
+        // Optional: Ping server every 30s to keep alive (Heroku idle timeout)
+        //    if (ws != null && ws.IsAlive)
+        //    {
+        //        // Keep-alive logic if needed
+        //    }
+
+        // IMPROVE THESE MAGIC NUMBERS!!
+        pv.transform.localPosition = new Vector3((sliderValuex - 50) * 0.0020f, (sliderValuey - 50) * 0.00075f, (sliderValuea - 50) * 0.001f + 0.15f);
+        pv.transform.eulerAngles = new Vector3(sliderValued-180, 90, -90);
+    }
 
     void OnApplicationQuit()
     {

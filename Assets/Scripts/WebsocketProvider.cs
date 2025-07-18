d1aac0fee87d3fecdf09f8778e53ad9fadfbb675
crunchycakes <85715474+crunchycakes@users.noreply.github.com>
2025-07-17 22:42:30 -0600

    project to both eyes

--- Assets/Scripts/WebsocketProvider.cs
+++ Assets/Scripts/WebsocketProvider.cs
@@ -11,8 +11,18 @@ public class WebsocketProvider : MonoBehaviour
     public float sliderValuey;
     public float sliderValuex;
     public float sliderValued;
+    public float sliderValuedo;
     public float sliderValuea;
+    public bool goodeyeisright;
+    
     public GameObject pv;
+
+    public GameObject MainCamera;
+    public GameObject OffCamera;
+
+    // vars for periodically ping to keep alive
+    private float time = 0.0f;
+    public float pingPeriod = 30.0f;
 
     void Start()
     {
@@ -55,6 +65,11 @@ public class WebsocketProvider : MonoBehaviour
                     sliderValued = val;
                     Debug.Log("slider d updated: " + sliderValued);
                 }
+                else if (sliderId == "sliderdo")
+                {
+                    sliderValuedo = val;
+                    Debug.Log("slider do updated: " + sliderValuedo);
+                }
                 else if (sliderId == "slidera")
                 {
                     sliderValuea = val;
@@ -72,15 +87,17 @@ public class WebsocketProvider : MonoBehaviour
                 //}
                 //Debug.Log("Transform success!?");
             }
+            else if ((string)msg["type"] == "goodeyeisright")
+            {
+                goodeyeisright = (bool)msg["value"];
+                Debug.Log("goodeyeisright changed: " + goodeyeisright);
+            }
         };
 
         // Called on errors
         ws.OnError += (sender, e) =>
         {
             Debug.LogError("!!!!! websocket error: " + e.Message);
-            Debug.Log(sliderValuex);
-            Debug.Log(sliderValuey);
-            Debug.Log(sliderValued);
         };
 
         // Called when closed
@@ -95,14 +112,40 @@ public class WebsocketProvider : MonoBehaviour
     void Update()
     {
         // Optional: Ping server every 30s to keep alive (Heroku idle timeout)
-        //    if (ws != null && ws.IsAlive)
-        //    {
-        //        // Keep-alive logic if needed
-        //    }
+        if (ws != null && ws.IsAlive)
+        {
+            time += Time.deltaTime;
+            if (time >= pingPeriod)
+            {
+                time = 0.0f;
+                ws.Send("unity keepalive");
+                Debug.Log("pinged to keep alive");
+            }
+        }
 
         // IMPROVE THESE MAGIC NUMBERS!!
-        pv.transform.localPosition = new Vector3((sliderValuex - 50) * 0.0020f, (sliderValuey - 50) * 0.00075f, (sliderValuea - 50) * 0.001f + 0.15f);
-        pv.transform.eulerAngles = new Vector3(sliderValued-180, 90, -90);
+        //pv.transform.localPosition = new Vector3((sliderValuex - 50) * 0.0020f, (sliderValuey - 50) * 0.00075f, (sliderValuea - 50) * 0.001f + 0.15f);
+        pv.transform.localPosition = new Vector3(0, 0, (sliderValuea - 50) * 0.001f + 0.15f);
+        if (goodeyeisright)
+        {
+            MainCamera.GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.Right;
+            OffCamera.GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.Left;
+        }
+        else
+        {
+            MainCamera.GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.Left;
+            OffCamera.GetComponent<Camera>().stereoTargetEye = StereoTargetEyeMask.Right;
+        }
+
+        OffCamera.transform.localPosition = new Vector3(-dioptersToDist(sliderValuex), -dioptersToDist(sliderValuey), 0);
+        OffCamera.transform.eulerAngles = new Vector3(0, 0, sliderValued-90);
+        //pv.transform.eulerAngles = new Vector3(sliderValued-180, 90, -90);
+    }
+
+    private float dioptersToDist(float diopters)
+    {
+        // TODO: FIX MAGIC NUMBER!!!
+        return (diopters / 100f) * 0.15f;
     }
 
     void OnApplicationQuit()
